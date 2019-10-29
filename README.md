a09c94152a8a96a2670a9d3843328ba7df395bec
Peter Goodspeed-Niklaus <peter.r.goodspeedniklaus@gmail.com>
2019-10-29 22:33:36 +0100

    implement getting an item from the DB

--- README.md
+++ README.md
@@ -4,3 +4,14 @@ Provide a simple `GET`/`SET` interface to DynamoDB.
 
 - `GET` is unauthenticated
 - `SET` depends on having the correct token
+
+## Notes
+
+- This code doesn't automatically provision any tables; that must happen
+externally.
+
+- We substantially simplify the schema. While DynamoDB is capable of
+having multiple keys of varying types and complex values, this project assumies
+that keys and values are always strings.
+
+- No length limit is applied to keys.
--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,17 @@
 use http::{Method, StatusCode};
 use lambda_http::{Body, IntoResponse, Request, RequestExt, Response};
 use lambda_runtime::{error::HandlerError, Context};
+use lazy_static::lazy_static;
+use maplit::hashmap;
+use rusoto_core::Region;
+use rusoto_dynamodb::{AttributeValue, DynamoDb, DynamoDbClient, GetItemInput};
 
 pub enum DispatchResult {
     IllegalMethod,
     UnknownPath,
+    DynamoErr,
+    Get(Option<String>),
     Set,
-    Get,
 }
 
 impl IntoResponse for DispatchResult {
@@ -20,6 +25,20 @@ impl IntoResponse for DispatchResult {
                 .status(StatusCode::NOT_FOUND)
                 .body(Body::Empty) // TODO: add helpful body text
                 .unwrap(),
+            Self::DynamoErr => Response::builder()
+                .status(StatusCode::INTERNAL_SERVER_ERROR)
+                .body(Body::Empty) // TODO: helpful body text about the dynamo problem
+                .unwrap(),
+            Self::Get(value) => match value {
+                Some(v) => Response::builder()
+                    .status(StatusCode::OK)
+                    .body(Body::Text(v))
+                    .unwrap(),
+                None => Response::builder()
+                    .status(StatusCode::NOT_FOUND)
+                    .body(Body::Empty)
+                    .unwrap(),
+            },
             _ => unimplemented!(),
         }
     }
@@ -47,9 +66,59 @@ pub fn dispatch(request: Request, ctx: Context) -> Result<impl IntoResponse, Han
     }
 }
 
+const ID: &'static str = "Id";
+const VALUE: &'static str = "Value";
+const TABLE: &'static str = "table";
+const KEY: &'static str = "key";
+
+lazy_static! {
+    // using the default region fetches it, if set, from AWS_DEFAULT_REGION
+    // or AWS_REGION environment variables. If unset or malformed, falls
+    // back to us-east-1.
+    static ref CLIENT: DynamoDbClient = DynamoDbClient::new(Region::default());
+}
+
 /// get an API key from a particular table and return it
 fn get(request: Request, _ctx: Context) -> Result<DispatchResult, HandlerError> {
-    unimplemented!()
+    let pparams = request.path_parameters();
+    let table: &str;
+    let key: &str;
+    match (pparams.get(TABLE), pparams.get(KEY)) {
+        (Some(t), Some(k)) => {
+            table = t;
+            key = k;
+        }
+        _ => return Ok(DispatchResult::UnknownPath),
+    }
+    let value = match CLIENT
+        .get_item({
+            let mut gii = GetItemInput::default();
+            gii.key = hashmap! {ID.to_string() => {
+                let mut av = AttributeValue::default();
+                av.s = Some(key.to_string());
+                av
+            }};
+            gii.table_name = table.to_string();
+            gii
+        })
+        .sync()
+    {
+        Ok(v) => v,
+        Err(_) => return Ok(DispatchResult::DynamoErr),
+    };
+
+    Ok(DispatchResult::Get(
+        value
+            .item
+            // TODO: can we consume av somehow so we can avoid the clone?
+            .map(|v| v.get(VALUE).map(|av| av.s.clone()))
+            // in the future, this map will be replaced with `.flatten()`,
+            // which requires nightly for now.
+            .map(|os| match os {
+                Some(Some(s)) => s,
+                _ => String::new(),
+            }),
+    ))
 }
 
 /// set a key to the provided byte stream
